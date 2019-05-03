c5a5f54bfe6db700e662efaa307b65ef2d953d76
Nai-Muh Dennis <naimuhdennis@gmail.com>
2019-05-03 15:41:19 -0700

    src folder

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import friends from "./friends.json";
+// import "./App.css";
+
+class App extends React.Component {
+  state = {
+    friends: friends,
+  };
+
+  handleRemove = (id) => {
+    const filteredFriends = this.state.friends.filter(f => f.id !== id);
+    this.setState ({ friends:filteredFriends});
+  };
+
+  render() {
+  return (
+    <Wrapper>
+      {this.state.friends.map(f =>
+         <FriendCard
+         image={f.image}
+         handleClick={() => this.handleRemove(f.id)}
+       />
+      )}
+    </Wrapper>
+  );
+}
+}
+
+export default App;
